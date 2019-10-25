3a2ec06a12a6e6cc541a5c7aa77a786902e5bc0b
François Hodierne <francois@hodierne.net>
2019-10-25 09:31:46 +0200

    debt: delete "api v1" payment method endpoint

--- server/routes.js
+++ server/routes.js
@@ -14,7 +14,6 @@ import { get } from 'lodash';
 import * as connectedAccounts from './controllers/connectedAccounts';
 import getHomePage from './controllers/homepage';
 import uploadImage from './controllers/images';
-import { createPaymentMethod } from './controllers/paymentMethods';
 import * as users from './controllers/users';
 import stripeWebhook from './controllers/webhooks';
 import * as email from './controllers/services/email';
@@ -192,13 +191,6 @@ export default app => {
    * Users.
    */
   app.get('/users/exists', required('email'), users.exists); // Checks the existence of a user based on email.
-
-  /**
-   * Create a payment method.
-   *
-   *  Let's assume for now a paymentMethod is linked to a user.
-   */
-  app.post('/v1/payment-methods', createPaymentMethod);
 
   /**
    * Separate route for uploading images to S3
